f1ce0ae1ca6476a168d1553ed6fff2677ed29dd6
Gabriel Faggione <gabriel.faggione@gmail.com>
2019-03-05 04:21:09 -0300

    Mudanças na movimentação, estruturas adicionadas e colliders funcionando

--- snd/main.py
+++ snd/main.py
@@ -5,6 +5,7 @@ from sprites import *
 from scenes import *
 
 class Game:
+    vec = pg.math.Vector2
     def __init__(self):
         # initialize game window, etc
         pg.init()
@@ -13,29 +14,60 @@ class Game:
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.running = True
+        self.cam = vec(WIDTH/2,HEIGHT/2)
+        self.camMarge = 10.0
+        self.font_name = pg.font.match_font(FONT_NAME)
     
     def new(self):
         # start a new game
         self.all_obj_scene = []
+        self.all_obj_ui = []
         self.all_sprites = pg.sprite.Group()
-        self.players = pg.sprite.Group()
         self.platforms = pg.sprite.Group()
-        self.background = pg.sprite.Group()
-        self.obstacles = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.stairs = pg.sprite.Group()
+        self.npcs = pg.sprite.Group()
         
-        self.p = Platform(0, 600, 1024, 10)
+        self.p = Platform(512, 100, 1024, 20)
         self.all_sprites.add(self.p)
         self.platforms.add(self.p)
         self.all_obj_scene.append(self.p)
+        self.w = Wall(380, 100, 20, 100)
+        self.all_sprites.add(self.w)
+        self.walls.add(self.w)
+        self.all_obj_scene.append(self.w)
+        self.pp = Platform(340, 5, 100, 20)
+        self.all_sprites.add(self.pp)
+        self.platforms.add(self.pp)
+        self.all_obj_scene.append(self.pp)
+        self.st = Stair(100, 80, 200, 150)
+        self.all_sprites.add(self.st)
+        self.stairs.add(self.st)
+        self.all_obj_scene.append(self.st)
+
+        teste = ["oi Goxtoso como vc vai?\nVc vem sempre aqui seu lindo cheiroso\nQue belo programador vc é", "uau\nnao é que funcionou mesmo?\nquem diria"]
+        self.npc = Npc("Roberto", 300, 80, teste)
+        self.all_sprites.add(self.npc)
+        self.npcs.add(self.npc)
+        self.all_obj_scene.append(self.npc)
+
         #spawner game objs das scenes
         #for obj in scene["objType"]:
         #    obj =  objType(parameters, gameref)
         #    self.all_sprites.add(obj)
         #    self.objType.add(obj)
 
+        self.mark = Mark()
+        self.all_obj_scene.append(self.mark)
+
         self.player = Player(self)
-        self.players.add(self.player)
+        self.all_obj_scene.append(self.player)
         self.all_sprites.add(self.player)
+
+        self.msgbox = TextBlock(self.player, self.screen)
+        self.all_sprites.add(self.msgbox)
+        self.all_obj_ui.append(self.msgbox)
+
         self.run()
     
     def run(self):
@@ -50,17 +82,39 @@ class Game:
     def update(self):
         # Game Loop - Update
         self.all_sprites.update()
-        # check if player hits a platform - only in falling
+        # check if player hits a platform
         self.player.rect.y += 1
-        hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+        platforms_hits = pg.sprite.spritecollide(self.player, self.platforms, False)
         self.player.rect.y -= 1
-        if hits:
-            self.player.pos.y = hits[0].rect.top + 1
-            self.player.vel.y = 0
+        if platforms_hits:
+            self.player.jumping = False
+            if self.player.vel.y > 0:
+                if self.player.pos.y < platforms_hits[0].rect.bottom:
+                    self.player.pos.y = platforms_hits[0].rect.top + 1
+                    self.player.vel.y = 0
+            if self.player.vel.y < 0:
+                if self.player.pos.y > platforms_hits[0].rect.bottom:
+                    if self.player.pos.x < platforms_hits[0].rect.left or self.player.pos.x > platforms_hits[0].rect.right: pass
+                    else: self.player.vel.y = 0
         
-        #for i in self.all_obj_scene:
-        #    i.rect.x -= self.player.vel.x
-        #    i.rect.y -= self.player.vel.y
+        # check if player hits a wall
+        walls_hits = pg.sprite.spritecollide(self.player, self.walls, False)
+        if walls_hits:
+            self.player.pos.x -= self.player.vel.x
+            self.player.vel.x = 0
+        
+        # check if player hits a npc
+        npcs_hits = pg.sprite.spritecollide(self.player, self.npcs, False)
+        if npcs_hits:
+            self.mark.toDraw = True
+            self.mark.rect.x = npcs_hits[0].rect.centerx
+            self.mark.rect.y = npcs_hits[0].rect.top - 10
+            self.mark.rect.midbottom = (self.mark.rect.x, self.mark.rect.y)
+        else:
+            self.mark.toDraw = False
+
+        self.cam.x -= self.player.vel.x
+        self.cam.y = (WIDTH/3 - self.player.pos.y)
     
     def events(self):
         # Game loop - Events
@@ -68,12 +122,34 @@ class Game:
             if event.type == pg.QUIT:
                 if self.playing: self.playing = False
                 self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_z:
+                    hits = pg.sprite.spritecollide(self.player, self.npcs, False)
+                    if hits and not self.player.onTalk:
+                        self.player.onTalk = True
+                        self.msgbox.index = 0
+                        self.msgbox.setMessage(hits[0].talk())
+                        self.msgbox.message = self.msgbox.message.splitlines()
+                    elif self.player.onTalk:
+                        if (self.msgbox.index + 1) < len(self.msgbox.message):
+                            self.msgbox.index += 1
+                        else:
+                            self.player.onTalk = False
+                            self.msgbox.index = 0
 
     def draw(self):
         #Game Loop - Draw
         self.screen.fill(BLACK)
-        self.all_sprites.draw(self.screen)
-
+        #self.all_sprites.draw(self.screen)
+        for i in self.all_obj_scene:
+            if i.toDraw:
+                self.screen.blit(i.image, (i.rect.x + self.cam.x , i.rect.y + self.cam.y))
+        for i in self.all_obj_ui:
+            if i.toDraw:
+                self.screen.blit(i.image, (i.rect.x, i.rect.y))
+        if self.player.onTalk:
+            self.drawLine(self.npc.name, 36, BLACK, self.msgbox.rect.topleft[0], self.msgbox.rect.topleft[1]+25)
+            self.drawMessage(22, RED, self.msgbox.rect.topleft[0], self.msgbox.rect.topleft[1])
         pg.display.flip()
     
     def show_start_screen(self):
@@ -83,6 +159,33 @@ class Game:
     def show_go_screen(self):
         pass
 
+    def drawLine(self, message, size, color, x, y):
+        font = pg.font.Font(FONT_NAME, size)
+        text_surface = font.render(message, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midleft = (x,y)
+        self.screen.blit(text_surface, text_rect)
+
+    def drawMessage(self, size, color, x, y):
+        font = pg.font.Font(FONT_NAME, size)
+        space = font.size(' ')[0]
+        paddingName = 25
+        margin = 50
+        x += margin
+        y += margin + paddingName
+        initX = x
+        for word in self.msgbox.message[self.msgbox.index].split(" "):
+            for i in range(0, len(word)):
+                text_surface = font.render(word[i], True, color)
+                width, height = text_surface.get_size()
+                text_rect = text_surface.get_rect()
+                if x > self.msgbox.rect.right - margin:
+                    x = initX
+                    y += height + space
+                text_rect.midleft = (x,y)
+                self.screen.blit(text_surface, text_rect)
+                x += width
+            x += space
 
 game = Game()
 game.show_start_screen()
--- snd/sprites.py
+++ snd/sprites.py
@@ -1,6 +1,7 @@
 # Sprite classes
 from settings import *
 import pygame as pg
+from copy import copy
 
 vec = pg.math.Vector2
 
@@ -21,36 +22,40 @@ class Player(pg.sprite.Sprite):
         self.image = pg.Surface((30,40))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
-        self.rect.center = 0 + CAMERAWIDTH, 0 + CAMERAHEIGHT
-        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.rect.center = 0, 0
+        self.jumping = False
+        self.onTalk = False
+        self.toDraw = True
+        self.pos = vec(0,0)
         self.vel = vec(0,0)
         self.acc = vec(0,0)
     
     def update(self):
         self.acc = vec(0,PLAYER_GRAV)
         keys = pg.key.get_pressed()
-        if keys[pg.K_UP]:
-            self.jump()
-        if keys[pg.K_LEFT]:
-            self.acc.x = -PLAYER_ACC
-        if keys[pg.K_RIGHT]:
-            self.acc.x = PLAYER_ACC
-        if keys[pg.K_LSHIFT]:
-            # apply friction
-            self.acc.x += (self.vel.x * PLAYER_FRICTION) * 2
-        else:
-            self.acc.x += self.vel.x * PLAYER_FRICTION
-        # equations of motion
+        if not self.onTalk:
+            if keys[pg.K_UP]:
+                hits = pg.sprite.spritecollide(self, self.game.stairs, False)
+                if hits:
+                    pass
+                self.jump()
+            if keys[pg.K_LEFT]:
+                self.acc.x = -PLAYER_ACC
+            elif keys[pg.K_RIGHT]:
+                self.acc.x = PLAYER_ACC
+            else:
+                self.vel.x = 0
+        self.acc.x += (self.vel.x * PLAYER_FRICTION)
         self.vel += self.acc
-        self.pos += self.vel + (0.5 * self.acc)
+        self.pos += self.vel
         self.rect.midbottom = self.pos
+        #print ("player pos", self.pos)
     
     def searchMove(self):
         first = pg.key.name(self.queue[0])
         if first == 'down':
             if self.queue in self.downList:
-                print ('oi')
-
+                pass
         
     def checkCall(self):
         self.time = pg.time.get_ticks()
@@ -61,25 +66,16 @@ class Player(pg.sprite.Sprite):
     
     def jump(self):
         # jump only if standing on a platform
-        self.rect.x += 1
-        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
-        self.rect.x -= 1
-        if hits:
-            self.vel.y -= 10
+        if not self.jumping and self.vel.y == 0:
+            self.rect.x += 1
+            hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+            self.rect.x -= 1
+            if hits and self.pos.y < hits[0].rect.bottom:
+                self.jumping = True
+                self.vel.y -= 10
 
-
+# -- Strutures --
 class Platform(pg.sprite.Sprite):
-    def __init__(self, x, y, w, h):
-        pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((w,h))
-        self.image.fill(RED)
-        self.rect = self.image.get_rect()
-        self.rect.x = x + CAMERAWIDTH
-        self.rect.y = y
-        self.rect.midbottom = (self.rect.x,self.rect.y)
-
-
-class Wall(pg.sprite.Sprite):
     def __init__(self, x, y, w, h):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((w,h))
@@ -87,4 +83,86 @@ class Wall(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.rect.midbottom = (self.rect.x,self.rect.y)
+        self.toDraw = True
+class Wall(pg.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((w,h))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
         self.rect.midbottom = (x,y)
+        self.toDraw = True
+class Stair(pg.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((w,h))
+        self.image.fill(LIGHTBLUE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.rect.midbottom = (x,y)
+        self.toDraw = True
+# -- End of Strutures
+
+
+class Mark(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((10,10))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
+        self.rect.midbottom = (0,0)
+        self.toDraw = False
+
+class Npc(pg.sprite.Sprite):
+    def __init__(self, name, x, y, posts):
+        pg.sprite.Sprite.__init__(self)
+        self.name = name
+        self.image = pg.Surface((30,40))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.rect.midbottom = (x,y)
+        self.posts = posts
+        self.postnum = 0
+        self.toDraw = True
+    
+    def talk(self):
+        msg = (self.posts[self.postnum])
+        self.postnum += 1
+        if self.postnum >= len(self.posts):
+            self.postnum = 0
+        return msg
+
+class TextBlock(pg.sprite.Sprite):
+    def __init__(self, player, screen):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((800,200))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = WIDTH / 2
+        self.rect.y = 700
+        self.rect.midbottom = (self.rect.x,self.rect.y)
+        self.max_x = self.rect.right - 20
+        self.player = player
+        self.screen = screen
+        self.toDraw = False
+        self.message = ""
+        self.font_name = pg.font.match_font(FONT_NAME)
+        self.index = 0
+    
+    def update(self):
+        if self.player.onTalk: self.toDraw = True
+        else: self.toDraw = False
+    
+    def setMessage(self, msg):
+        self.message = msg
+        
+
+    
